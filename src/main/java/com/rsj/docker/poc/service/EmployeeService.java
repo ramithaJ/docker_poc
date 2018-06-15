f14e74abf41c667dbbe529c50e2dfea3d54e6a41
Ramitha <ramitha.su@gmail.com>
2018-06-15 15:04:47 -0400

    remove application properties and add yml

--- src/main/java/com/rsj/docker/poc/service/EmployeeService.java
+++ src/main/java/com/rsj/docker/poc/service/EmployeeService.java
@@ -1,9 +1,13 @@
 package com.rsj.docker.poc.service;
 
+import com.rsj.docker.poc.config.Config;
 import com.rsj.docker.poc.model.Address;
 import com.rsj.docker.poc.model.ContactCard;
 import com.rsj.docker.poc.model.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -19,15 +23,21 @@ public class EmployeeService {
         return employeeList;
     }
 
+
+    @Autowired
+    private Config config;
+
+
     @Value("${employee.type}")
     private String employeeType;
+
 
     private  Employee getEmployee(){
 
         Employee employee = new Employee();
         employee.setEmployeeId("0000001");
         employee.setBusinessLine("Banking");
-        employee.setEmployeeType(employeeType);
+        employee.setEmployeeType(config.getMessage());
         employee.setEmployeeName("PNC Bank");
         employee.setAddress(getAddress());
         employee.setContactCard(getContactCard());
