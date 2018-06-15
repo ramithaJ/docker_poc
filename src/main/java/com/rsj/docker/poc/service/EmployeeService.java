5c54bb8a6bd15f8a58f37116038ccd7c36b3553e
Ramitha <ramitha.su@gmail.com>
2018-06-15 16:22:30 -0400

    remove application properties and add yml

--- src/main/java/com/rsj/docker/poc/service/EmployeeService.java
+++ src/main/java/com/rsj/docker/poc/service/EmployeeService.java
@@ -48,7 +48,7 @@ public class EmployeeService {
     }
 
 
-    @Scheduled(fixedDelay = 5000)
+    @Scheduled(fixedDelay = 1)
     public void hello() {
         System.out.println("The message is: " + config.getMessage());
     }
